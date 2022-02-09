9945ec409c532c33303fa8ccbb833060e9007609
Nils Kruthoff <nils.kruthoff@iubh-fernstudium.de>
2022-02-09 22:37:49 +0100

    Refactoring code

--- js/search.js
+++ js/search.js
@@ -1,33 +1,39 @@
 window.addEventListener("load", function() {
     const form = document.getElementById('searchForm')
 
+    // adding a submit event to the search and lucky button and executes an anonymous function
     form.addEventListener("submit", function(e) {
-        e.preventDefault(); // before the code
-        /* do what you want with the form */
+        e.preventDefault();
+
+        // getting data attributes and form data
         const source = document.querySelector('#searchField').dataset.source;
         const formData = document.querySelectorAll('#searchForm input');
         const searchKeyword = formData[0]['value'];
         let searchQuery = "";
 
+        // if the search button was clicked
         if (e['submitter']['id'] === 'searchSubmit')
         {
-            console.log("TEST")
+            // if it is from normal google search
             if (source === "search")
             {
+                // set this query string
                 searchQuery = "https://www.google.com/search?q=" + searchKeyword;
+            // if it is from image search
             } else if (source === "image-search")
             {
+                // set this query string to forward to google pictures
                 searchQuery = "https://www.google.com/search?q=" + searchKeyword + "&tbm=isch";
             }
-            console.log(e);
+        // if the feeling lucky button is pressed
         } else if (e['submitter']['id'] === 'searchLucky')
         {
+            // set this query string to forward to google first search result
             searchQuery = "https://www.google.de/search?q=" + searchKeyword + "&btnI=Auf gut Glück!"
         }
 
         console.log("Redirected to " + searchQuery);
+        // redirect to search results page with search query as url
         window.location.href = searchQuery;
     })
 });
-
-//https://www.google.de/search?q=test&btnI=Auf gut Glück!&
